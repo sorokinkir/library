9e680255b145976c95b914523877b414ef058197
itari <itari@yandex.ru>
2016-05-12 16:47:12 +0600

    Создан непосредственно сам класс для вывода списка новостей

--- news/views.py
+++ news/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.views.generic import ListView
+from .models import News
 
 
-def index(request):
-    return render(request, 'news_list.html', {})
+class IndexView(ListView):
+    model = News
+    template_name = 'news_list.html'
