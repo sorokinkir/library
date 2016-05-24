d9199d0258472ff8eb3e486417f20fee709c084c
Kirill Sorokin <cheshirski.eve@gmail.com>
2016-05-24 12:29:01 +0700

    url на новость

--- news/urls.py
+++ news/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import url
-from .views import IndexView
+from .views import IndexView, DetailNews
 
 
 urlpatterns = [
-    url(r'^$', IndexView.as_view(), name='index')
+    url(r'^$', IndexView.as_view(), name='index'),
+    url(r'^(?P<slug>[\w-]+)/$', DetailNews.as_view(), name='news-detail'),
 ]
