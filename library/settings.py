92d798a271f059f8e38fac64c7fdc76c5b6e2d4c
Kirill Sorokin <cheshirski.eve@gmail.com>
2016-06-01 14:28:01 +0700

    чтобы slug работал, сделал отдельно news url

--- library/settings.py
+++ library/settings.py
@@ -39,8 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'authors.apps.AuthorsConfig',
     'news.apps.NewsConfig',
+    'authors.apps.AuthorsConfig',
 
     # Батарейки
 
